31d9e0f6fa619eb763131da533607db8c72bf0d0
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-09-24 22:06:07 +0200

    Consumables

--- objects/gameplay/player/consumables/consumable.gd
+++ objects/gameplay/player/consumables/consumable.gd
@@ -1,2 +1,8 @@
 extends Resource 
 class_name Consumables 
+
+@export var s : Array[StatTuple] 
+
+func Consume(stats : Stats) -> void:
+    for i in s:
+        stats.Set(i.amount+stats.Get(i.type),i.type)
