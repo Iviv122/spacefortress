8dddfb6a31b8d4cd983d2020e1fcc9bf439a57b3
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-09-28 20:49:43 +0200

    win screen

--- objects/enemy/spawner.gd
+++ objects/enemy/spawner.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Spawner
 
 @export var wave_composition : WaveComposition 
+@export var win_screen : PackedScene
 @export var radius : float 
 
 var wave : Wave
@@ -12,10 +13,13 @@ var index = 0
 
 func _ready():
 
-	wave = wave_composition.waves[PlayerInstance.misc_stats.wave_count] 
-	queue  = wave.enemy_kinds.duplicate()
-	queue.shuffle()
-
+	if PlayerInstance.misc_stats.wave_count <= wave_composition.waves.size()-1:
+		wave = wave_composition.waves[PlayerInstance.misc_stats.wave_count] 
+		queue  = wave.enemy_kinds.duplicate()
+		queue.shuffle()
+	else:
+		get_tree().call_group("load_scene","load_scene",win_screen)
+	
 	current_reload = 1
 
 var initPos : Vector2 = Vector2(0,0)
